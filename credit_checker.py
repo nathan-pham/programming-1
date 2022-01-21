eeceb4bbe894dfa8135dd1f4f9ba0282b024fd1b
Nathan Pham <nathanpham.me@gmail.com>
2022-01-21 10:07:10 -0800

    absolute cancer

--- credit_checker.py
+++ credit_checker.py
@@ -4,6 +4,4 @@
 # Name:         Nathan Pham
 
 
-digit_sum = lambda n : sum([int(i) for i in list(str(n))])
-validate = lambda n: digit_sum(''.join(str(digit_sum(n[i]) * 2) if i % 2 == 0 else n[i] for i in range(len(n)))) % 10 == 0
-print("Valid credit card" if validate(''.join("3141 4352 7221 1006".split(' '))) else "Invalid credit card")
+digit_sum = lambda n : sum([int(i) for i in list(str(n))]); validate = lambda n: digit_sum(''.join(str(digit_sum(n[i]) * 2) if i % 2 == 0 else n[i] for i in range(len(n)))) % 10 == 0; print("Valid credit card" if validate(''.join("3141 4352 7221 1006".split(' '))) else "Invalid credit card")
