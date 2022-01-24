6c11259f84f9fc0fccc416bac54793595690c9a2
Nathan Pham <nathanpham.me@gmail.com>
2022-01-24 09:32:46 -0800

    validate phone numbers

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ Del Oro High School Programming 1 class
 * [Password Checker](password_checker.py)
 * [Review: Strings and Loops](review_strings_loops.py)
 * [Credit Card Checker](credit_checker.py)
+* [Phone Number Checker](phone_number_checker.py)
 
 ## Helping Students
 * [Discord Bot](discord_bot.py)
---
+++ phone_number_checker.py
@@ -0,0 +1,34 @@
+# Filename:     phone_number_checker.py
+# Date:         1/24/2022
+# Purpose:      Validate phone number format
+# Name:         Nathan Pham
+
+import re
+
+
+# remove special chars and whitespace
+def remove_formatting(phone_number: str) -> str:
+    return re.sub(r"[()-.\s]", "", phone_number)
+
+
+# re-add special chars and whitespace
+def add_formatting(phone_number: str) -> str:
+    return f"({phone_number[:3]}) {phone_number[3:6]} - {phone_number[6:-1]}"
+
+
+# main method
+def main() -> None:
+    while True:
+        phone_number = input("Enter a phone number > ")
+
+        if phone_number == "quit":
+            break
+
+        phone_number = remove_formatting(phone_number)
+
+        print(f"Phone number: {add_formatting(phone_number)}" if len(
+            phone_number) == 10 else "Invalid phone number.")
+
+
+if __name__ == "__main__":
+    main()
