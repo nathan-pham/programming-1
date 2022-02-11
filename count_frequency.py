0fad677e4e9a4a350eb8bffd39b9de088d1e62cb
Nathan Pham <nathanpham.me@gmail.com>
2022-02-11 09:23:00 -0800

    count word frequency

--- count_frequency.py
+++ count_frequency.py
@@ -4,8 +4,31 @@
 # Name:         Nathan Pham
 
 
+from typing import Dict, Any
+
+
 def prompt(message: str) -> str:
     return input(message).lower()
+
+
+def count_char_frequency(message: str) -> Dict[Any, int]:
+    """
+    Count the number of chars in a string
+    """
+
+    frequency = {}
+    for char in message: frequency[char] = frequency.get(char, 0) + 1
+    return frequency
+
+
+def count_word_frequency(message: str) -> Dict[Any, int]:
+    """
+    Count the number of words in a string
+    """
+    
+    frequency = {}
+    for word in message.replace("\n", " ").split(): frequency[word] = frequency.get(word, 0) + 1
+    return frequency
 
 
 def main() -> None:
---
+++ file_count_frequency.py
@@ -0,0 +1,43 @@
+# Filename:     file_count_frequnecy.py
+# Date:         2/11/2022
+# Purpose:      Count the number of words in a file
+# Name:         Nathan Pham
+
+
+from typing import Tuple, List, Dict, Any
+
+from count_frequency import count_word_frequency
+from heap_sort import random_sort
+
+
+def dict_to_params(frequency: Dict[Any, int]) -> Tuple[List[str], List[int]]:
+    """
+    Convert a dictionary to a list of parameters (keys & values for random_sort)
+    """
+
+    keys = []
+    values = []
+
+    for key, value in frequency.items():
+        keys.append(key)
+        values.append(value)
+
+    return keys, values
+
+
+def print_frequency(labels: List[str], scores: List[int]) -> None:
+    """
+    Print the frequency of each word in the file
+    """
+
+    for i in range(len(labels)):
+        print("%-20s %s" % (labels[i], scores[i]))
+
+
+def main() -> None:
+    with open("./files/i_too_poem.txt") as file:
+        frequency = count_word_frequency(file.read())
+        print_frequency(*random_sort(*dict_to_params(frequency)))
+
+if __name__ == "__main__":
+    main()
